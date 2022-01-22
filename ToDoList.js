5141c807c3b3ae5228fc96388564571f873eeff6
JingYi-XU <zmmouoasus@gmail.com>
2022-01-23 01:27:42 +0800

    加入add, remove:function 都還未完成 - 23_1_22

--- ToDoList.js
+++ ToDoList.js
@@ -3,5 +3,31 @@ $(window).on('load', function(){
 });
 
 $(document).ready(function(){
+new Vue({
+    el:'#list',
+    data:{
+        listItem:'',        /* 用來儲存user輸入的項目 */
+        items:[{
+            // id: '',
+            // text:  ''
+        }]
+    },
+    methods:{
+        /* 新增任務 */
+        add:function(){
+            var text = this.listItem;
+            var id = Math.floor(Date.now());
+            this.items.push({
+                id: id,
+                text: text
+            });
+            this.listItem = '';
+        },
 
+        /* 刪除任務 */
+        remove:function(item){
+            this.items.splice(item, 1);
+        }
+    }    
+})
 });
